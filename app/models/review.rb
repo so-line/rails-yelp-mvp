edc803aa33e2794fa9dd02635b40794657b2b000
soline dziadzio <soline.dziadzio@gmail.com>
2023-02-22 12:30:03 +0100

    Prepare rails app with external specs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { in: [0..5], only_integer: true }
 end
