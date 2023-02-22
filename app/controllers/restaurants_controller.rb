b9011eb16644fabc9c4b3673f86e146f3c1d8e96
soline dziadzio <soline.dziadzio@gmail.com>
2023-02-22 15:41:25 +0100

    restaurant view-controller-routes finalized

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,21 +3,39 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
-  end
-
-  def show
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path(@restaurant)
+    raise
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path(@restaurant)
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ('/')
   # root 'articles#index'
 
-  # READ  ALL
-  get 'restaurants', to: 'restaurants#index'
-
-  # CREATE
-  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  post 'restaurants', to: 'restaurants#create'
-
-  # READ ONE - The `show` route needs to be *after* `new` route.
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-
-  # UPDATE
-  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
-  patch 'restaurants/:id', to: 'restaurants#update'
-
-  # DELETE
-  delete 'restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
