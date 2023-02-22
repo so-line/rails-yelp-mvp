998824f5718da0740cd39f6cafa0924d4e6879fd
soline dziadzio <soline.dziadzio@gmail.com>
2023-02-22 12:59:35 +0100

    finished models restaurant and reviews with validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { in: [0..5], only_integer: true }
+  validates :rating, presence: true, numericality: { in: (0..5), only_integer: true }
 end
---
+++ db/migrate/20230222115411_change_data_type_for_phone_number.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ChangeDataTypeForPhoneNumber < ActiveRecord::Migration[7.0]
+  def change
+    change_column :restaurants, :phone_number, :string
+  end
+end
